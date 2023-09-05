5e0d4355d09201cbec4675b57b9b1d7baaf61037
hzj_jie <hzj_jie@hotmail.com>
2023-09-05 01:18:07 -0700

    build.sh

--- 赤色回忆/build.sh
+++ 赤色回忆/build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+chmod 777 赤色回忆.txt
 rm 赤色回忆.txt
 for i in $(ls -1 *.txt); do
+    echo >> 赤色回忆.partial
+    echo $i >> 赤色回忆.partial
     cat $i >> 赤色回忆.partial
 done
 
 mv 赤色回忆.partial 赤色回忆.txt
+chmod 444 赤色回忆.txt
